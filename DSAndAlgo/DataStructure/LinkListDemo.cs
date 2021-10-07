85fa75a4a3a8cb626c217c0380ab9b2409888c00
Shashank Agrawal <33977537+agrawalshashank@users.noreply.github.com>
2021-10-07 17:25:13 +0530

    Add files via upload
    first commit

---
+++ DSAndAlgo/DataStructure/LinkListDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LinkedList.SimpleLinkedList;
+
+namespace DSAndAlgo.DataStructure
+{
+    public class LinkListDemo
+    {
+        public void SimpleLinkedList()
+        {
+            SimpleLinkList<string> simpleLinkList = new SimpleLinkList<string>();
+            simpleLinkList.AddLastWithOptimization("One");
+            simpleLinkList.AddLastWithOptimization("two");
+            simpleLinkList.AddLastWithOptimization("Three");
+            simpleLinkList.AddLastWithOptimization("Four");
+
+            simpleLinkList.AddFirst("First Item");
+
+            simpleLinkList.ReadAll();
+        }
+    }
+}
